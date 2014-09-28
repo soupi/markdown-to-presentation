92eac47d2a661e7cca0078050286cf14bd46eba2
Gil Mizrahi <gil.mizrahi@intel.com>
2014-09-28 09:09:03 +0300

    init

---
+++ README.md
@@ -0,0 +1,37 @@
+Markdown to presentation
+========================
+
+A simple converter from markdown to html presentation
+
+
+How To:
+------
+1. Create a Markdown file and seperate slides with '---' (html's <hr> tag)
+2. Use m2p tool (haskell compiler and cabal needed to compile m2p) to create an HTML file from your Markdown and place it in the output folder
+
+You can also use any other md2html converter. just add the following to the html:
+```html
+
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/github.css">
+<link rel="stylesheet" type="text/css" href="highlight/styles/solarized_light.css"> <!-- Or your preferable syntax highlight theme -->
+
+<script src="js/jquery-1.11.0.min.js"></script>
+<script src="highlight/highlight.pack.js"></script>
+<script>hljs.initHighlightingOnLoad();</script>
+<script src="js/present.js"></script>
+```
+
+Actually, you can omit everything but:
+```html
+<script src="js/jquery-1.11.0.min.js"></script>
+<script src="js/present.js"></script>
+```
+
+but then styling is up to you.
+
+Packages and Libraries used to create m2p:
+------------------------------------------
+- [markdown](http://hackage.haskell.org/package/markdown)
+- [highlight.js](https://highlightjs.org/)
+- [a slighty modified github.css](https://gist.github.com/andyferra/2554919)
---
+++ src/Main.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import System.Environment
+import qualified Text.Markdown as MD
+import qualified Data.Text.Lazy as T()
+import qualified Data.Text.Lazy.IO as TIO
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+import Text.Blaze.Html5 ((!))
+import Text.Blaze.Html.Renderer.Text as HR
+
+main :: IO ()
+main = do
+  args <- getArgs
+  if getNumFromList 2 args then (TIO.readFile (args !! 0) >>= return . HR.renderHtml  . (template (args !! 0)). (MD.markdown MD.def) >>= TIO.writeFile (args !! 1))
+    else putStrLn "[Usage: m2h <src> <dest>]"
+
+getNumFromList :: Int -> [a] -> Bool
+getNumFromList 0 _ = True
+getNumFromList _ [] = False
+getNumFromList n (_:xs) = getNumFromList (n-1) xs
+
+
+template :: String -> H.Html -> H.Html
+template title htm = do
+  H.docTypeHtml $ do
+    H.head $ do
+      H.title (H.toHtml title)
+      H.link ! A.rel "stylesheet" ! A.type_ "text/css" ! A.href "css/style.css"
+      H.link ! A.rel "stylesheet" ! A.type_ "text/css" ! A.href "css/github.css"
+      H.link ! A.rel "stylesheet" ! A.type_ "text/css" ! A.href "highlight/styles/solarized_light.css"
+    H.body $ do
+      H.div ! A.class_ "container" $ do
+        htm
+      H.script ! A.src "js/jquery-1.11.0.min.js" $ ""
+      H.script ! A.src "highlight/highlight.pack.js" $ ""
+      H.script ! A.src "js/present.js" $ ""
+      H.script $ "hljs.initHighlightingOnLoad();"
